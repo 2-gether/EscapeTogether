b4e0fce10b3c0f3981915a8ce20a4f010b0c5a99
MaximeJumeaux <jumeaux.maxime@outlook.fr>
2020-06-27 19:36:42 +0200

    ?

---
+++ Assets/Scripts/Network/CustomNetworkHUD.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+using TMPro;
+using System.Text.RegularExpressions;
+
+public class CustomNetworkHUD : MonoBehaviour {
+
+    NetworkManager manager;
+
+    void Awake() {
+        manager = GetComponent<NetworkManager>();
+    }
+
+    public void Join(TextMeshProUGUI ip) {
+
+        if (ip.text.ToLower() == "localhost") manager.networkAddress = ip.text.ToLower();
+        else {
+            Regex ipFormated = new Regex(@"\b\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}\b");
+            MatchCollection result = ipFormated.Matches(ip.text);
+            manager.networkAddress = result[0].ToString();
+        }
+        manager.StartClient();
+        Init();
+    }
+
+    public void Host() {
+        manager.StartHost();
+        Init();
+    }
+
+    public void Server() {
+        manager.StartServer();
+        Init();
+    }
+
+    public void Exit() {
+        Application.Quit();
+    }
+
+    void Init() {
+        if (NetworkClient.isConnected && !ClientScene.ready) {
+            ClientScene.Ready(NetworkClient.connection);
+
+            if (ClientScene.localPlayer == null) {
+                ClientScene.AddPlayer(NetworkClient.connection);
+            }
+        }
+    }
+}
