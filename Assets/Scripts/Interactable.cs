97e49a2b8392dab61e6bb8d8167e38b614988a6a
MaximeJumeaux <jumeaux.maxime@outlook.fr>
2020-06-27 19:13:25 +0200

    mini fix

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -5,7 +5,16 @@ using UnityEngine;
 public class Interactable : MonoBehaviour {
 	[SerializeField] float radius = 3f;
 
+	[SerializeField] GameObject hover;
+
 	public float Radius { get => radius; }
+
+	public void SetHover(bool isHover) {
+		if(hover != null)
+			hover.SetActive(isHover);
+	}
+
+	public virtual void Action() { }
 
 	void OnDrawGizmos() {
 		Gizmos.color = Color.yellow;
