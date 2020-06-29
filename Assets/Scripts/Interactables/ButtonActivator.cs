cf5777b7bcf35479d2a59c8cbe8d54565a09dda5
MaximeJumeaux <jumeaux.maxime@outlook.fr>
2020-06-29 14:01:11 +0200

    Add first steps of timer buttons
    #9

---
+++ Assets/Scripts/Interactables/ButtonActivator.cs
@@ -0,0 +1,32 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonActivator : Activator {
+	[SerializeField] float time = 2f;
+	bool isUsed = false;
+
+	public override void Action(NetworkIdentity player) {
+		if(!isUsed) {
+			isUsed = true;
+			foreach(Actionable a in targets)
+				a.Action();
+			StartCoroutine(Countdown());
+		}
+	}
+
+	IEnumerator Countdown() {
+		yield return new WaitForSeconds(time);
+		isUsed = false;
+		foreach(Actionable a in targets)
+			a.Action();
+	}
+
+	public override bool CanBeUsed() {
+		bool targetsReady = true;
+		foreach(Actionable a in targets)
+			targetsReady &= a.CanBeActioned();
+		return !isUsed && targetsReady;
+	}
+}
