8b8eb118b5f6adaa9c71eb7f679b5ecc66dbabe5
MaximeJumeaux <jumeaux.maxime@outlook.fr>
2020-07-04 16:40:34 +0200

    #5

--- Assets/Scripts/E_ditor/FileSystemFolder.cs
+++ Assets/Scripts/E_ditor/FileSystemFolder.cs
@@ -4,49 +4,51 @@ using System.Collections.Generic;
 using UnityEngine.UI;
 public class FileSystemFolder : MonoBehaviour {
 
-    bool isExpanded = false;
-    [SerializeField] GameObject children;
-    [SerializeField] Sprite selectedSprite;
-    [SerializeField] Sprite unselectedSprite;
-    [SerializeField] FileSystemFolder[] baseFolders;
+	bool isExpanded = false;
+	[SerializeField] GameObject children;
+	[SerializeField] Sprite selectedSprite;
+	[SerializeField] Sprite unselectedSprite;
+	[SerializeField] FileSystemFolder[] baseFolders;
 
-    List<FileSystemFolder> folder = new List<FileSystemFolder>();
-    Image im;
-    void Start() {
-        im = GetComponent<Image>();
-        foreach (Transform go in children.transform) {
-            FileSystemFolder folderChild = go.GetComponentInChildren<FileSystemFolder>();
-            if (folderChild != null) {
-                folder.Add(folderChild);
-            }
-        }
-    }
+	List<FileSystemFolder> folder = new List<FileSystemFolder>();
+	Image im;
+	void Start() {
+		im = GetComponent<Image>();
+		foreach(Transform go in children.transform) {
+			FileSystemFolder folderChild = go.GetComponentInChildren<FileSystemFolder>();
+			if(folderChild != null) {
+				folder.Add(folderChild);
+			}
+		}
+	}
 
-    public void Click() {
-        if (isExpanded) Reduce();
-        else Expand();
-    }
+	public void Click() {
+		if(isExpanded)
+			Reduce();
+		else
+			Expand();
+	}
 
-    public void Expand() {
-        children.SetActive(true);
-        isExpanded = true;
-        // change sprite of the folder
-        im.sprite = selectedSprite;
-        foreach (var f in baseFolders) {
-            if (f != this)
-                f.Reduce();
-        }
-    }
+	public void Expand() {
+		children.SetActive(true);
+		isExpanded = true;
+		// change sprite of the folder
+		im.sprite = selectedSprite;
+		foreach(var f in baseFolders) {
+			if(f != this)
+				f.Reduce();
+		}
+	}
 
-    public void Reduce() {
-        isExpanded = false;
-        Debug.Log(im);
-        im.sprite = unselectedSprite;
-        foreach (var f in folder) {
-            f.Reduce();
-        }
-        children.SetActive(false);
-        // change sprite of the folder
+	public void Reduce() {
+		isExpanded = false;
+		if(im != null)
+			im.sprite = unselectedSprite;
+		foreach(var f in folder) {
+			f.Reduce();
+		}
+		children.SetActive(false);
+		// change sprite of the folder
 
-    }
+	}
 }
